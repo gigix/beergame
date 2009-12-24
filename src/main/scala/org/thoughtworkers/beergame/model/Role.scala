0672477aa96599c4ffb5759c30b2e227c91bd345
Gigix <user@thoughtworkss-macbook-pro-6.local>
2009-12-24 23:09:54 +0800

    associate upstream and downstream roles

--- src/main/scala/org/thoughtworkers/beergame/model/Role.scala
+++ src/main/scala/org/thoughtworkers/beergame/model/Role.scala
@@ -1,5 +1,20 @@
 package org.thoughtworkers.beergame.model
 
 class Role(_name: String) {
-	def name() = _name
+	var _downstream: Role = null
+	var _upstream: Role = null
+	
+	val name = _name
+	
+	def downstream = _downstream
+	def upstream = _upstream
+	
+	def setUpstream(role: Role) {
+		_upstream = role
+		role.setDownstream(this)
+	}
+	
+	private def setDownstream(role: Role) {
+		_downstream = role
+	}
 }
--- src/test/scala/org/thoughtworkers/beergame/model/RoleTest.scala
+++ src/test/scala/org/thoughtworkers/beergame/model/RoleTest.scala
@@ -7,7 +7,22 @@ import Assert._
 class RoleTest extends TestCase("role") {
 	def test_create_role_with_name() {
 		var retailer = new Role("Retailer")
-		assertTrue(retailer != Nil)
+		assertTrue(retailer != null)
 		assertEquals("Retailer", retailer.name)
+	}
+	
+	def test_associate_downstream_and_upstream_roles() {
+		var retailer = new Role("Retailer")
+		var consumer = new Role("Consumer")
+		var wholesaler = new Role("Wholesaler")
+		
+		retailer.setUpstream(wholesaler)
+		consumer.setUpstream(retailer)
+		
+		assertEquals(retailer.downstream, consumer)
+		assertEquals(retailer.upstream, wholesaler)
+		
+		assertEquals(consumer.upstream, retailer)
+		assertEquals(wholesaler.downstream, retailer)
 	}
 }
