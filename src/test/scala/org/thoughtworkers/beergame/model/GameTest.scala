b8b092dff4978435ec0f89a772f03f597e3781ea
Gigix <user@thoughtworkss-macbook-pro-6.local>
2009-12-25 00:03:10 +0800

    missed game test. commit it.

---
+++ src/test/scala/org/thoughtworkers/beergame/model/GameTest.scala
@@ -0,0 +1,10 @@
+package org.thoughtworkers.beergame.model
+
+import _root_.junit.framework._
+import Assert._
+
+class GameTest extends TestCase("game") {
+	def test_create_game_with_roles() {
+		// TODO: add test here
+	}
+}
