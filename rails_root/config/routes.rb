3ec2982e5514b95ab7d62d181c17c05388b54518
Gigix <user@ThoughtWorkss-MacBook-Pro.local>
2010-02-08 14:34:08 +0800

    map web root to games controller

--- rails_root/config/routes.rb
+++ rails_root/config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => 'games'
+  
   map.resources :games, :has_many => {:roles => :orders}
   # The priority is based upon order of creation: first created -> highest priority.
 
